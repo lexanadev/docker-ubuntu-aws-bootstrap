ecd9f83be489f0df46805f45005a2dd10fc42179
Tiago de Oliveira Stutz <tiagostutz@gmail.com>
2017-08-07 08:30:10 -0300

    Adicionado login no registry.lexana.com.br

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ sh get-docker.sh
 
 sudo curl -fsSL https://raw.githubusercontent.com/lexana/zac-aws-bootstrap/master/docker.config -o /etc/init/docker.conf
 sudo service docker restart
+
+docker login -u lexana -p 6z2M185RFZmfC1udoV https://registry.lexana.com.br:5043
