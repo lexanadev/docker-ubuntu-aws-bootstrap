9f24120feb6da4dc248f38d26456e3ef0ac6ef65
Tiago de Oliveira Stutz <tiagostutz@gmail.com>
2017-08-17 02:46:13 -0300

    password =\

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo sh get-docker.sh
 sudo curl -fsSL https://raw.githubusercontent.com/lexana/zac-aws-bootstrap/master/docker.config -o /etc/init/docker.conf
 sudo service docker restart
 
-#docker login -u lexana -p 6z2M185RFZmfC1udoV https://registry.lexana.com.br:5043
+#docker login -u lexana -p [pass] https://registry.lexana.com.br:5043
 
 sudo mv sshd_config /etc/ssh/sshd_config
 sudo service ssh restart
