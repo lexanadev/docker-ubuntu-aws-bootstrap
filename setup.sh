19c7911f76d14e4f15f4d4887757fb4363374751
Tiago de Oliveira Stutz <tiagostutz@gmail.com>
2018-06-20 08:32:48 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo sh get-docker.sh
 sudo curl -fsSL https://raw.githubusercontent.com/lexana/zac-aws-bootstrap/master/docker.config -o /etc/init/docker.conf
 sudo service docker restart
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 #docker login -u lexana -p [pass] https://registry.lexana.com.br:5043
